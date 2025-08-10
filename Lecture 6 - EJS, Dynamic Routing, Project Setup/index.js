b4fcca0e8d50ba9011c4e6b7e45cd49061bdfbab
Nimra <nimraiqbal512@gmail.com>
2025-08-10 12:55:16 +0500

    Lecture 6 - dynamic routing

--- Lecture 6 - EJS, Dynamic Routing, Project Setup/index.js
+++ Lecture 6 - EJS, Dynamic Routing, Project Setup/index.js
@@ -1,25 +1,52 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// // These parsers help us to use form
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+// // console.log(__dirname);
+// // console.log(__dirname + '/public')
+// app.use(express.static(path.join(__dirname, 'public')));  //All static files(Images, videos, .css files, .js files) would be in 'public' folder. 
+
+// //ejs pages will be rendered
+// app.set('view engine', 'ejs');
+
+// // First route
+// app.get("/", (req, res)=>{
+//     // res.send("Its working");
+//     res.render("index");    //This page 'index or index.ejs' must be in 'views'
+// });
+
+// // giving callback with server
+// app.listen(3000, ()=>{
+//     console.log('Ya Ali Madad');
+// })
+
+
+// ===========================
+// Dynamic routing
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-// These parsers help us to use form
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-// console.log(__dirname);
-// console.log(__dirname + '/public')
-app.use(express.static(path.join(__dirname, 'public')));  //All static files(Images, videos, .css files, .js files) would be in 'public' folder. 
-
-//ejs pages will be rendered
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-// First route
-app.get("/", (req, res)=>{
-    // res.send("Its working");
-    res.render("index");    //This page 'index or index.ejs' must be in 'views'
+app.get("/profile/:username", (req, res)=>{
+    console.log(req.params.username);
+    // res.send('Working ' + req.params.username);
+    res.send(`Working, ${req.params.username}`);
 });
 
-// giving callback with server
+app.get("/profile/:username/:age", (req, res)=>{
+    res.send(`Working, ${req.params.username}, ${req.params.age}`);
+});
+
 app.listen(3000, ()=>{
-    console.log('Ya Ali Madad');
+    console.log("Ya Ali Madad");
 })
