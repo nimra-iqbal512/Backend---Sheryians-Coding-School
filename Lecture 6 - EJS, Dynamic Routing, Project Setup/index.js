c19c20ca3f9a05d32373ceb7721286eaba97cc08
Nimra <nimraiqbal512@gmail.com>
2025-08-10 12:17:31 +0500

    Lecture 6 - ejs and static files

--- Lecture 6 - EJS, Dynamic Routing, Project Setup/index.js
+++ Lecture 6 - EJS, Dynamic Routing, Project Setup/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // These parsers help us to use form
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// console.log(__dirname);
+// console.log(__dirname + '/public')
+app.use(express.static(path.join(__dirname, 'public')));  //All static files(Images, videos, .css files, .js files) would be in 'public' folder. 
 
 //ejs pages will be rendered
 app.set('view engine', 'ejs');
 
 // First route
 app.get("/", (req, res)=>{
-    res.send("Its working");
+    // res.send("Its working");
+    res.render("index");    //This page 'index or index.ejs' must be in 'views'
 });
 
 // giving callback with server
