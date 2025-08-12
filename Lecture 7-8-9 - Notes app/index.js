b72def28a8e8e7f85735184ed27cba709c3d0d8a
Nimra <nimraiqbal512@gmail.com>
2025-08-12 16:49:51 +0500

    Lecture 7 - Employing Tailwind CSS for the frontend aspect

--- Lecture 7-8-9 - Notes app/index.js
+++ Lecture 7-8-9 - Notes app/index.js
@@ -5,10 +5,10 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(Path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res)=>{
-    res.send("Welcome");
+    res.render("index");
 })
 
 app.listen(3000, ()=>{
