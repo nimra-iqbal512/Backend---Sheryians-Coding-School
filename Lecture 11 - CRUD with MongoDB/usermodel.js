e76113a911d290a70c99acb459e40ea918bc832b
Nimra <nimraiqbal512@gmail.com>
2025-08-22 16:22:39 +0500

    Lecture 11 - Mongoose Schema

--- Lecture 11 - CRUD with MongoDB/usermodel.js
+++ Lecture 11 - CRUD with MongoDB/usermodel.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
+
+// Details of user
+const userSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    email: String,
+});
