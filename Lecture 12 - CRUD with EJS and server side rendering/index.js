1ca0cadd08de7738a83cf8fe18824e503421f538
Nimra <nimraiqbal512@gmail.com>
2025-09-05 16:16:30 +0500

    Lecture 12 - Setting up public and views folder

--- Lecture 12 - CRUD with EJS and server side rendering/index.js
+++ Lecture 12 - CRUD with EJS and server side rendering/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
-    res.send('Hello');
+    res.render('index');
 })
 
 app.listen(3000, ()=>{
