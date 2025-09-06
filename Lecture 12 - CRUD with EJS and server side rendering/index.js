849754adbdb88659f1541023e260e68582784855
Nimra <nimraiqbal512@gmail.com>
2025-09-06 18:22:15 +0500

    Lecture 12 - create user in database

--- Lecture 12 - CRUD with EJS and server side rendering/index.js
+++ Lecture 12 - CRUD with EJS and server side rendering/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
+const { name } = require('ejs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -14,6 +16,17 @@ app.get('/', (req, res)=>{
 app.get('/read', (req, res)=>{
     res.render('read');
 })
+
+app.post('/create', async (req, res)=>{
+    let {name, email, image} = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image, 
+    })
+    res.send(createdUser);
+})
+
 app.listen(3000, ()=>{
     console.log('Ya Ali a.s Madad');
 })
