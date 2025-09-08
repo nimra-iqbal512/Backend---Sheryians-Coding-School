6a1a20d5efe409ca23c19e4f91e289a8188c895a
Nimra <nimraiqbal512@gmail.com>
2025-09-08 21:25:05 +0500

    Lecture 15 - project basic setup

--- Lecture 15 - Authentication and Authorization - JWT - MongoDB/index.js
+++ Lecture 15 - Authentication and Authorization - JWT - MongoDB/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+// Set ejs as view engine
+app.set('view engine', 'ejs');
+// To read forms
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// To read static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
 app.get('/', (req, res)=>{
-    res.send('Hello World');
+    res.render('index');
 })
 
 app.listen(3000, ()=>{
