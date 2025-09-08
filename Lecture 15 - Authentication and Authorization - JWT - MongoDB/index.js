9e5f18fb0d4aef75c6fa912ddacf26bd6f2112a3
Nimra <nimraiqbal512@gmail.com>
2025-09-08 22:02:33 +0500

    Lecture 15 - create user in DB without password encryption

--- Lecture 15 - Authentication and Authorization - JWT - MongoDB/index.js
+++ Lecture 15 - Authentication and Authorization - JWT - MongoDB/index.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const app = express();
+const userModel = require("./models/user");
 
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-// Set ejs as view engine
 app.set('view engine', 'ejs');
-// To read forms
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// To read static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 app.get('/', (req, res)=>{
     res.render('index');
+})
+
+app.post('/create', async (req, res)=>{
+    let {username, email, password, age} = req.body;
+    let createdUser = await userModel.create({
+        username,
+        email,
+        password,
+        age,
+    });
+    res.send(createdUser);
 })
 
 app.listen(3000, ()=>{
