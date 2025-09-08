e915e68aa608bda890070d2baae0fa005a2e31b2
Nimra <nimraiqbal512@gmail.com>
2025-09-08 09:41:23 +0500

    Lecture 14 - password encryption using bcrypt

--- Lecture 14 - Authentication and Authorization - Bcrypt - JWT/index.js
+++ Lecture 14 - Authentication and Authorization - Bcrypt - JWT/index.js
@@ -1,17 +1,18 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-
-app.use(cookieParser());
+const bcrypt = require('bcrypt');
 
 app.get('/', (req, res)=>{
-    // Set cookie
-    res.cookie("name", "nimra");    //Once this cookie is set in browser. Ab browser sy koi b request ae gi, to yh cookie b us request k sath khud hi chali jaye gi 
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash('this password', salt, (err, hash)=>{
+            console.log(salt);
+            console.log(hash);
+        })
+    } );
     res.send("done");
 })
 
 app.get('/read', (req, res)=>{
-    console.log(req.cookies);   //kiu k browser mai cookies already store hai. Ab jesy hi browser sy '/read' request nikly gi, to yeh cookie b sath jaye gi, is liye hum is cookies ko is path py b read kr skty
     res.send('Read page');
 })
 
