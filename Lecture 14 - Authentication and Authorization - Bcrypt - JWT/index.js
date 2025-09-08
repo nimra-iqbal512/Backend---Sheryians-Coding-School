26d157aac71ac8d4a350e4a159cae494f8210644
Nimra <nimraiqbal512@gmail.com>
2025-09-08 09:16:33 +0500

    Lecture 14 - Read cookies

--- Lecture 14 - Authentication and Authorization - Bcrypt - JWT/index.js
+++ Lecture 14 - Authentication and Authorization - Bcrypt - JWT/index.js
@@ -1,10 +1,18 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
+app.use(cookieParser());
+
 app.get('/', (req, res)=>{
     // Set cookie
-    res.cookie("name", "nimra");
+    res.cookie("name", "nimra");    //Once this cookie is set in browser. Ab browser sy koi b request ae gi, to yh cookie b us request k sath khud hi chali jaye gi 
     res.send("done");
+})
+
+app.get('/read', (req, res)=>{
+    console.log(req.cookies);   //kiu k browser mai cookies already store hai. Ab jesy hi browser sy '/read' request nikly gi, to yeh cookie b sath jaye gi, is liye hum is cookies ko is path py b read kr skty
+    res.send('Read page');
 })
 
 app.listen(3000, ()=>{
