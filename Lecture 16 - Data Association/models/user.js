f4452dc37d130b951094c0005145532eeb7a304b
Nimra <nimraiqbal512@gmail.com>
2025-09-10 08:26:15 +0500

    Lecture 16 - Referencing data, Link user and post with each other

--- Lecture 16 - Data Association/models/user.js
+++ Lecture 16 - Data Association/models/user.js
@@ -3,10 +3,18 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/data');
 
 const userSchema = mongoose.Schema({
-    username: String,
+    // username: String,
+    username: { //username can also be written like this
+        type: String
+    },
     email: String,
     age: Number,
-    posts: Array, 
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'post'
+        }
+    ], 
 })
 
 module.exports = mongoose.model('user', userSchema);
