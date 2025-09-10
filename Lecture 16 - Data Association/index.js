7636ebce6fd34d9fce64b8d50ee9e72e3a87d88f
Nimra <nimraiqbal512@gmail.com>
2025-09-10 08:13:40 +0500

    Lecture 16 - Create user in DB

--- Lecture 16 - Data Association/index.js
+++ Lecture 16 - Data Association/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+
 app.get('/', (req, res)=>{
     res.send('Hello World');
+});
+
+app.get('/create', async (req, res)=>{
+    let user = await userModel.create({
+        username: "Nimra",
+        age: 25,
+        email: "nimra@gmail.com"
+    });
+    res.send(user);
 });
 
 app.listen(3000, ()=>{
