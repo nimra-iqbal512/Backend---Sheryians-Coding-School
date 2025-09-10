ec9eef3aa6d7a360e04d0a203480508ff90c1b06
Nimra <nimraiqbal512@gmail.com>
2025-09-10 08:44:43 +0500

    Lecture 16 - Create post, and referencing/linking post and user

--- Lecture 16 - Data Association/index.js
+++ Lecture 16 - Data Association/index.js
@@ -17,6 +17,21 @@ app.get('/create', async (req, res)=>{
     res.send(user);
 });
 
+app.get("/post/create", async (req, res)=>{
+    // yha hum post kop btae gy, k user kn hai
+    let post = await postModel.create({
+        postdata: "Hello World",
+        user: "68c0f29d9d7e2ad54eb1025d",   //abi hum hard code kr rhy hain, but Logged In user ki id yaha ae gi
+    });
+    
+    // user ko b pta ho k us ny kon c post create ki hai
+    let user = await userModel.findOne({_id: '68c0f29d9d7e2ad54eb1025d'});
+    user.posts.push(post._id);
+    await user.save();  //If we made changes without findOneAndUpdate(), then we have to use user.save() to save the changes
+
+    res.send({post, user});
+});
+
 app.listen(3000, ()=>{
     console.log('Ya Ali a.s');
 });
