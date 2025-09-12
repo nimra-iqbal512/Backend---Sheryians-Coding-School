9ae7aee5b508e2aaa48eddef37ebc51628598dda
Nimra <nimraiqbal512@gmail.com>
2025-09-12 12:40:00 +0500

    Lecture 17 - Project basic setup

--- Lecture 17 - Mini Project - Data Association - MongoDB/index.js
+++ Lecture 17 - Mini Project - Data Association - MongoDB/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-
 const userModel = require('./models/user');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 app.get('/', (req, res)=>{
-    res.send('Hello.........');
+    res.render('index');
 });
 
 app.listen(3000, ()=>{
