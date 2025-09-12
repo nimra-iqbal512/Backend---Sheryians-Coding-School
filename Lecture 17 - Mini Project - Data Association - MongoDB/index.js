dcb775eafbd42fae76c8d0d6223d42934285b49f
Nimra <nimraiqbal512@gmail.com>
2025-09-12 20:37:55 +0500

    Lecture 17 - Register user, GUI and backend

--- Lecture 17 - Mini Project - Data Association - MongoDB/index.js
+++ Lecture 17 - Mini Project - Data Association - MongoDB/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const userModel = require('./models/user');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userModel = require('./models/user');
+const postModel = require('./models/post'); //require krny sy mongodb mai model create ho jata hai 
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -13,6 +17,31 @@ app.use(cookieParser());
 app.get('/', (req, res)=>{
     res.render('index');
 });
+
+app.post('/register', async (req, res)=>{
+    let {email, password, username, name, age} = req.body;  
+
+    // Check either user exists or not
+    let user = await userModel.findOne({email: req.body.email});
+    if(user) return res.status(500).send("User already registered");
+
+    // Create user if it does not exist
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async (err, hash)=>{
+            let user = await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash
+            });
+
+            let token = jwt.sign({email: email, userid: user._id}, 'secret');
+            res.cookie("token", token);
+            res.send("Registered");
+        })
+    })
+})
 
 app.listen(3000, ()=>{
     console.log('Ya Ali a.s');
