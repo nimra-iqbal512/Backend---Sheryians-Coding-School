1785482c18a6db350921f766e1be400c04de8d48
Nimra <nimraiqbal512@gmail.com>
2025-09-15 21:25:19 +0500

    Lecture 20 - creating upload route to handle upload image backend logic

--- Lecture 20 - Multer - multipart - formdata/index.js
+++ Lecture 20 - Multer - multipart - formdata/index.js
@@ -4,38 +4,43 @@ const path = require('path');
 const crypto = require('crypto');
 const multer = require('multer');
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'publis')));
+
+
+crypto.randomBytes(12, (err, bytes) => {
+    console.log(bytes.toString("hex"));
+})
 
 // Copy 'disk storage' from 'npm multer'
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/uploads')
-  },
-  filename: function (req, file, cb) {
-    // console.log(file.originalname);
-    
-    crypto.randomBytes(12, (err, bytes)=>{
-        const fn = bytes.toString("hex") + path.extname(file.originalname);
-        cb(null, fn);
-    })
-  }
+    destination: function (req, file, cb) {
+        cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        // console.log(file.originalname);
+
+        crypto.randomBytes(12, (err, bytes) => {
+            const fn = bytes.toString("hex") + path.extname(file.originalname);
+            cb(null, fn);
+        })
+    }
 })
 const upload = multer({ storage: storage })
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('test');
 });
 
-app.post('/upload', (req, res)=>{
+app.post('/upload', upload.single('image'), (req, res) => {
     console.log(req.body);  // Empty object. REASON: Multer adds a body object and a file or files object to the request object. The body object contains the values of the text fields of the form, the file or files object contains the files uploaded via the form.
     console.log(req.file);
-    
+
     res.redirect('/');
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Ya Ali a.s Madad');
 })
