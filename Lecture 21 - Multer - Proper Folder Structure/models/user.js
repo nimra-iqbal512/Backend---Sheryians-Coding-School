602ab65b3af80b295c1826b2925d5efd7e4414c2
Nimra <nimraiqbal512@gmail.com>
2025-09-19 11:10:05 +0500

    Lecture 21 - creating user model to save image

---
+++ Lecture 21 - Multer - Proper Folder Structure/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://127.0.0.1:27017/profilePic`);
+
+let userSchema = mongoose.Schema({
+    username: String,
+    profilepic: {
+        type: String,
+        default: "default.png"
+    },
+});
+
+module.exports = mongoose.model('user', userSchema);
