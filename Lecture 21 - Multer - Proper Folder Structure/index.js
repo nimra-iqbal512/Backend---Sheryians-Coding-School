a1ea026628cdc53512452fe4e099c8b565c592c3
Nimra <nimraiqbal512@gmail.com>
2025-09-19 12:19:07 +0500

    Lecture 21 - update image in database

--- Lecture 21 - Multer - Proper Folder Structure/index.js
+++ Lecture 21 - Multer - Proper Folder Structure/index.js
@@ -10,21 +10,34 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', async (req, res)=>{
+app.get('/create', async (req, res)=>{
     let user = await userModel.create({
         username: 'nimrazzz',
     })
     console.log(user);
-    
-    res.render('index', {user});
+    res.redirect('/');
+})
+
+app.get('/', async (req, res)=>{
+    let user = await userModel.findOne({username: 'nimrazzz'});
+    if(user){
+        res.render('index', {user});
+    }else{
+        res.redirect('/create');
+    }
 })
 
 app.get("/profile/upload", (req, res)=>{
     res.render("profileUpload");
 })
 
-app.post("/upload", multerconfig.single("image"), (req, res)=>{
-    console.log(req.file);
+app.post("/upload", upload.single("image"), async (req, res)=>{
+    console.log(req.file.filename);
+    
+    let user = await userModel.findOne({username: 'nimrazzz'}); //Update username with the id or email of logged in user
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect('/');
 })
 
 app.listen(3000, ()=>{
